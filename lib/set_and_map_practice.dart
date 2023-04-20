8bc5ca12f0b0d06d386b08fec54ed0cdc4eb7f67
Vikas Mishra <vikasmishra99195@gmail.com>
2023-04-20 11:01:49 +0530

    Set and Map practice code in Dart

---
+++ lib/set_and_map_practice.dart
@@ -0,0 +1,138 @@
+void main(){
+
+  // # Set : Set is a unordered collection of unique items.
+
+  // var set1 = {'Vikas', 'Shiv', 'Arjun', 'Krsna', 'Karn', 'Bheem'};
+  // print(set1);
+  // print(set1.runtimeType);
+
+  // var set2 = {'Vikas', 'Shiv', 'Arjun', 'Krsna', 'Karn', 'Bheem', 1, 2};
+  // print(set2);
+  // print(set2.runtimeType);
+
+  // var set3 = <String>{'Vikas', 'Shiv', 'Arjun', 'Krsna', 'Karn', 'Bheem'};
+  // print(set3);
+  // print(set3.runtimeType);
+
+  // Set set4 = {'Vikas', 'Shiv', 'Arjun', 'Krsna', 'Karn', 'Bheem'};
+  // print(set4);
+  // print(set4.runtimeType);
+
+  // Set<String> set5 = {'Vikas', 'Shiv', 'Arjun', 'Krsna', 'Karn', 'Bheem'};
+  // print(set5);
+  // print(set5.runtimeType);
+
+  // var set6 = <String>{};
+  // set6.add("Vikas");
+  // print(set6);
+
+  // var set7 = {};           ## it is map not list
+  // print(set7);
+  // print(set7.runtimeType);
+
+
+  // # Map :
+
+  // ## Map Literal
+
+  // var address = {
+  //   'name':'Vikas',
+  //   'Class':'MCA',
+  //   'City':'Bsp',
+  //   'State':'CG',
+  //   'no': 296765356
+  // };
+
+  // print(address);
+  // print(address.runtimeType);
+
+  // Map<String, String> details = {
+  //   'name':'Vikas',
+  //   'Class':'MCA',                   ## Map<String, String> = support only String- 1st String show key, 2nd one show Pair
+  //   'City':'Bsp',
+  //   'State':'CG'
+  // };
+  // print(details);
+
+  // Map<String, Object> details = {
+  //   'name':'Vikas',
+  //   'Class':'MCA',                //   ## Map<String, Object> = support only String- 1st String show key, 2nd one show Pair
+  //   'City':'Bsp',
+  //   'State':'CG',
+  //   'No' : 89787786
+
+  //   };
+  //   print(details);
+
+  // Map<String, int> details = {
+  //   'stud id':8787,
+  //   'userid':0647,                //   ## Map<String, String> = support only String- 1st String show key, 2nd one show Pair in int
+  //   'pass':6353,
+  //   'wp no':7377,
+  //   'contact' : 89787786
+
+  // };
+  // print(details);
+
+  // ## Empty Map
+  // map() Constructor
+
+  // var name = Map();
+  // print(name);
+  // print(name.runtimeType);
+  //
+  // Map<String, String> name1 = Map();
+  // print(name1);
+  // print(name1.runtimeType);
+
+  // ## Add item in map
+
+  // var name1 = {};
+  // name1['Name'] = 'Vikas';
+  // name1['Address'] = 'Satna';
+  // name1['phone'] = 98989897;
+  // print(name1);
+  // print(name1.runtimeType);
+
+  // ## Access Map Element
+
+  // var address = {
+  //     'name':'Vikas',
+  //     'Class':'MCA',
+  //     'City':'Bsp',
+  //     'State':'CG',
+  //     'no': 296765356
+  //   };
+
+  //   print(address);
+  //   print(address.runtimeType);
+
+  // print(address['name']);
+  // print(address['Class']);
+  // print(address['City']);
+  // print(address['State']);
+  // print(address['no']);
+
+  // ## Map Properties And Methods
+
+  // var address = {
+  //       'name':'Vikas',
+  //       'Class':'MCA',
+  //       'City':'Bsp',
+  //       'State':'CG',
+  //       'no': 296765356
+  //     };
+
+  // print(address);
+  // print(address.runtimeType);
+  // print(address.length);
+  // print(address.values);
+  // print(address.keys);
+  // print(address.isEmpty);
+  // print(address.isNotEmpty);
+  // print(address.entries);
+  // print(address.containsKey('Pin'));
+  // print(address.containsValue('Value'));
+
+
+  }
